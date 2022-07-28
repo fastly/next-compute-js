08d20ec4dfb0cce4cb3d25d3ae73e22914a8b83f
Katsuyuki Omuro <komuro@fastly.com>
2022-07-28 14:06:15 +0900

    cli initial commit

---
+++ src/bin/next-compute-js.ts
@@ -0,0 +1,4 @@
+#!/usr/bin/env node
+
+const cli = require('../cli/index');
+cli.exec();
---
+++ src/init-server/generate.ts
@@ -0,0 +1,47 @@
+import path from "path";
+import fs from "fs";
+
+const COMPUTE_JS_DIR = './compute-js';
+
+// Directory structure looks weird, but /src gets compiled to /build/src,
+// so are one layer deeper
+const RESOURCES_DIR = '../../../resources';
+
+export function checkServerProject() {
+  const computeJsDir = path.resolve(COMPUTE_JS_DIR);
+  return fs.existsSync(computeJsDir);
+}
+
+export function copyResourceFile(filePath: string, src: string, target: string) {
+  const srcFilePath = path.resolve(__dirname, RESOURCES_DIR, src, filePath);
+  const targetFilePath = path.resolve(target, filePath);
+  fs.copyFileSync(srcFilePath, targetFilePath);
+}
+
+export function generateServerProject() {
+
+  if(checkServerProject()) {
+    console.error(`❌ '${COMPUTE_JS_DIR}' directory already exists!`);
+    process.exit(1);
+  }
+
+  const computeJsDir = path.resolve(COMPUTE_JS_DIR);
+  console.log("Initializing Compute@Edge Application in " + computeJsDir + "...");
+  fs.mkdirSync(computeJsDir);
+  fs.mkdirSync(path.resolve(computeJsDir, './src'));
+
+  // Copy resource files
+  const files = [
+    'src/index.js',
+    'src/polyfill.js',
+    '.gitignore',
+    'default-content-types.cjs',
+    'fastly.toml',
+    'package.json',
+    'static-publish.rc.js',
+    'webpack.config.js',
+  ];
+  for (const file of files) {
+    copyResourceFile(file, 'compute-js', computeJsDir);
+  }
+}
