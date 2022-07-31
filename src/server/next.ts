8b4a4a9a44f18b1b532d9e05eb439714181bf592
Katsuyuki Omuro <komuro@fastly.com>
2022-08-01 01:53:09 +0900

    Improve API surface a bit

--- src/server/next.ts
+++ src/server/next.ts
@@ -2,21 +2,57 @@ import { ComputeJsServerOptions } from "./common";
 import { loadConfig } from "./config";
 import { PHASE_PRODUCTION_SERVER } from "next/constants";
 import NextComputeJsServer from "./next-compute-js-server";
+import { ComputeJsNextRequest, ComputeJsNextResponse } from "./base-http/compute-js";
 
-export default async function createServer(options: ComputeJsServerOptions) {
+class NextServer {
+  public options: ComputeJsServerOptions;
+  public server: NextComputeJsServer | null;
 
-  const conf = await loadConfig(
-    PHASE_PRODUCTION_SERVER,
-    options.computeJs.assets,
-    options.dir ?? '.',
-    options.conf,
-  );
+  constructor(options: ComputeJsServerOptions) {
+    this.options = options;
+    this.server = null;
+  }
 
-  console.log({conf});
+  async getServer() {
+    if(this.server != null) {
+      return this.server;
+    }
+    const conf = await loadConfig(
+      PHASE_PRODUCTION_SERVER,
+      this.options.computeJs.assets,
+      this.options.dir ?? '.',
+      this.options.conf,
+    );
 
-  return new NextComputeJsServer({
-    ...options,
-    conf,
-  });
+    this.server = new NextComputeJsServer({
+      ...this.options,
+      conf,
+    });
 
+    return this.server;
+  }
+
+  async getRequestHandler() {
+    return (await this.getServer())
+      .getRequestHandler();
+  }
+
+  async handleFetchEvent(event: FetchEvent) {
+    const nextRequest = new ComputeJsNextRequest(event.request, event.client);
+    const nextResponse = new ComputeJsNextResponse();
+
+    const requestHandler = await this.getRequestHandler();
+    await requestHandler(nextRequest, nextResponse);
+
+    return await nextResponse.toResponse();
+  }
+
+}
+
+export default async function createServer(
+  options: ComputeJsServerOptions
+) {
+  const server = new NextServer(options);
+  await server.getServer(); // In C@E there is no sense in lazy loading this
+  return server;
 }
