6031634cf2345652c4f2e58ae0a8d79af4673edb
Katsuyuki Omuro <komuro@fastly.com>
2022-08-01 00:31:12 +0900

    Make config loader async

--- src/server/next.ts
+++ src/server/next.ts
@@ -3,9 +3,9 @@ import { loadConfig } from "./config";
 import { PHASE_PRODUCTION_SERVER } from "next/constants";
 import NextComputeJsServer from "./next-compute-js-server";
 
-export default function createServer(options: ComputeJsServerOptions) {
+export default async function createServer(options: ComputeJsServerOptions) {
 
-  const conf = loadConfig(
+  const conf = await loadConfig(
     PHASE_PRODUCTION_SERVER,
     options.dir ?? '.',
     options.conf,
