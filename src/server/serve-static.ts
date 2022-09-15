683913f8918795413cb94cbe3904d4c2cd917442
Katsuyuki Omuro <komuro@fastly.com>
2022-09-15 10:04:44 +0900

    Copy existing headers

--- src/server/serve-static.ts
+++ src/server/serve-static.ts
@@ -4,6 +4,7 @@
  */
 
 import accepts from 'accepts';
+import type { ServerResponse } from 'http';
 
 import { Assets } from './common';
 import { getAssetContentType, readAssetFile } from './require';
@@ -25,27 +26,46 @@ export async function serveStatic(
 
   const decodedPath = decodeURIComponent(path);
   const asset = readAssetFile(assets, decodedPath, dir);
-  const contentType = getAssetContentType(assets, decodedPath, dir);
+
+  const outgoingHeaders = new Headers();
+
+  // Copy all the headers that have already been set on this response
+  // for example those set by setResponseHeaders()
+  const nodeRes = res.originalResponse as ServerResponse;
+  for (const [key, value] of Object.entries(nodeRes.getHeaders())) {
+    if(value == null) {
+      continue;
+    }
+    if(Array.isArray(value)) {
+      for (const entry of value) {
+        outgoingHeaders.append(key, entry);
+      }
+    } else {
+      outgoingHeaders.append(key, String(value));
+    }
+  }
+
+  if(!outgoingHeaders.has('Content-Type')) {
+    outgoingHeaders.append('Content-Type',
+      getAssetContentType(assets, decodedPath, dir)
+    );
+  }
 
   let response = new Response(asset, {
     status: 200,
-    headers: {
-      'Content-Type': contentType,
-    },
+    headers: outgoingHeaders,
   });
 
   if(compress) {
     const accept = accepts(req.originalRequest);
     const encoding = accept.encodings(['gzip', 'deflate']) as 'gzip' | 'deflate' | false;
     if (encoding) {
+      outgoingHeaders.append('Content-Encoding', encoding);
       response = new Response(
         response.body!.pipeThrough(new CompressionStream(encoding)),
         {
           status: 200,
-          headers: {
-            'Content-Type': contentType,
-            'Content-Encoding': encoding,
-          },
+          headers: outgoingHeaders,
         }
       );
     }
